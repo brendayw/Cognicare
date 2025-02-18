52c8dd65764131298344cd39c45f01d4a7f38344
Brenda Yañez <brendayw97@gmail.com>
2025-02-18 11:30:03 -0300

    paneles profesional terminados

--- public/js/profesional.js
+++ public/js/profesional.js
@@ -36,14 +36,14 @@ const myChart = new Chart(ctx, {
         labels: nombresPacientes, // Las etiquetas del gráfico (nombres de los pacientes)
         datasets: [{
             label: 'Avance del Tratamiento (sesiones)', // El nombre del dataset
-            data: avancesTtoFull,
+            data: avancecsDxFull,
             backgroundColor: 'rgb(255, 205, 86)', // Color de fondo de las barras
             borderColor: 'rgba(194, 191, 42, 0.73)', // Color del borde de las barras
             borderWidth: 1
         },
         {
             label: 'Avances del Diagnostico (sesiones)',
-            data: avancecsDxFull,
+            data: avancesTtoFull,
             backgroundColor: 'rgb(54, 162, 235)',
             borderColor: 'rgba(42, 164, 194, 0.73)',
             borderWidth: 1
@@ -52,6 +52,13 @@ const myChart = new Chart(ctx, {
     options: {
         indexAxis: 'y',
         scales: {
+            x: {
+                grid: {
+                    display: false,
+                    drawBorder: false,
+                },
+                max: 10 // Limita el máximo al 100%
+            },
             y: {
                 beginAtZero: true,
                 grid: {
@@ -65,4 +72,54 @@ const myChart = new Chart(ctx, {
             }
         }
     }
+});
+
+/*panel de editar perfil*/
+document.addEventListener('DOMContentLoaded', function() {
+    const panelProfesional = document.querySelector(".panel_profesional");
+    const panelEditar = document.querySelector(".panel_editar");
+    const btnEditar = document.querySelector("#btnEditar");
+    const btnVolver = document.querySelector("#btnVolver");
+
+    panelProfesional.classList.add("visible");
+    panelEditar.classList.add("hidden");
+
+    btnEditar.addEventListener("click", function () {
+        panelProfesional.classList.remove("visible");
+        panelProfesional.classList.add("hidden");
+
+        panelEditar.classList.remove("hidden");
+        panelEditar.classList.add("visible");
+    });
+
+
+    btnVolver.addEventListener("click", function () {
+        panelEditar.classList.remove("visible");
+        panelEditar.classList.add("hidden");
+
+        panelProfesional.classList.remove("hidden");
+        panelProfesional.classList.add("visible");
+    });
+
+    const addTimeBtn = document.querySelector('.add-time');
+
+    addTimeBtn.addEventListener('click', function() {
+        const horariosDiv = document.getElementById('horarios');
+        const newHorarioGroup = document.createElement('div');
+        const newInput = document.createElement('input');
+        newInput.type = 'time';
+        newInput.name = 'horarios[]';
+        newInput.classList.add('hora');
+        newHorarioGroup.appendChild(newInput);
+        const deleteBtn = document.createElement('button');
+        deleteBtn.type = 'button';
+        deleteBtn.textContent = 'Eliminar hora';
+        deleteBtn.classList.add('delete-time');
+        
+        deleteBtn.addEventListener('click', function() {
+            horariosDiv.removeChild(newHorarioGroup);
+        });
+        newHorarioGroup.appendChild(deleteBtn);
+        horariosDiv.appendChild(newHorarioGroup);
+    });
 });
